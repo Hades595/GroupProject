819404e24754f7c047d270cc2476031f0bad0b22
Chirag Mehta <cm298@students.waikato.ac.nz>
2021-10-06 17:14:42 +1300

    Added the ability to draw the player  Need to work on drawing the other objects

--- app/src/main/java/com/example/groupproject/Ball.java
+++ app/src/main/java/com/example/groupproject/Ball.java
@@ -1,11 +1,27 @@
 package com.example.groupproject;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
 public class Ball extends Object{
 
     //Player
 
+    private int x;
+    private int y;
+    private int radius;
+    private Paint color = new Paint(Color.BLACK);
+
     public Ball(int x, int y, int radius) {
         super(x, y, radius);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
 
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawCircle(x, y, radius, color);
     }
 }
--- app/src/main/java/com/example/groupproject/Obstacles.java
+++ app/src/main/java/com/example/groupproject/Obstacles.java
@@ -3,11 +3,10 @@ package com.example.groupproject;
 public class Obstacles extends Object {
 
     //Interaction with the player
-    //Can throw them off course and power ups etc
+    //Can throw them off-course and power ups etc
 
     public Obstacles(int x, int y, int radius){
         super(x,y, radius);
-
     }
 
 }
