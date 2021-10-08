d8ce3ce5ac26aaea61a0c0eb86496c96d9dd5857
Chirag Mehta <cm298@students.waikato.ac.nz>
2021-10-08 21:34:54 +1300

    Implemented full-screen  Might find a way to do it without repeating code later

--- app/src/main/java/com/example/groupproject/GameActivity.java
+++ app/src/main/java/com/example/groupproject/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.groupproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -108,6 +109,17 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //Remove the action bar
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+
+        //Set immersive mode, since user will be clicking a lot
+        int uiOptions = View. SYSTEM_UI_FLAG_IMMERSIVE
+                | View. SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
 
         //New graphics view for drawing ball
         GraphicsView graphicsView = new GraphicsView(this);
